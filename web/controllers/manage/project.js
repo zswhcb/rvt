d929c620f405b42764253fe2f225d7c690c62617
unknown <3203317@qq.com>
2015-12-08 17:17:56 +0800

    haha

--- web/controllers/manage/project.js
+++ web/controllers/manage/project.js
@@ -7,10 +7,42 @@
 
 var util = require('speedt-utils');
 
-var conf = require('../../settings');
+var conf = require('../../settings'),
+	EventProxy = require('eventproxy');
 
 var biz = {
+	user: require('../../../biz/user'),
 	project: require('../../../biz/project')
+};
+
+/**
+ *
+ * @params
+ * @return
+ */
+exports.addUI = function(req, res, next){
+	// TODO
+	var ep = EventProxy.create('users', function (users){
+		// TODO
+		res.render('manage/project/Add', {
+			conf: conf,
+			title: '新增 | '+ req.query.name +' | '+ conf.corp.name,
+			description: '',
+			keywords: ',html5',
+			data: {
+				users: users
+			}
+		});
+	});
+
+	ep.fail(function (err){
+		next(err);
+	});
+
+	biz.user.findAll(function (err, docs){
+		if(err) return ep.emit('error', err);
+		ep.emit('users', docs);
+	});
 };
 
 /**
