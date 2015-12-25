982ae1e557d3b65fdaf4159bfef8827b2423d347
unknown <3203317@qq.com>
2015-12-25 17:47:25 +0800

    haha

--- TelCall/src/tel/call/broadcast/PhoneBroadcastReceiver.java
+++ TelCall/src/tel/call/broadcast/PhoneBroadcastReceiver.java
@@ -1,20 +1,9 @@
 package tel.call.broadcast;
 
-import java.net.URLEncoder;
 import java.util.Date;
-import java.util.HashMap;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import tel.call.R;
-import tel.call.action.ServiceAction;
 import tel.call.util.AppUtil;
-import tel.call.util.HttpUtil;
-import tel.call.util.HttpUtil.RequestMethod;
-import tel.call.util.RestUtil;
 import tel.call.util.UserInfo;
-import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -22,8 +11,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.database.Cursor;
-import android.os.Handler;
-import android.os.Message;
 import android.provider.CallLog.Calls;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
@@ -44,7 +31,6 @@ public class PhoneBroadcastReceiver extends BroadcastReceiver {
 	private OnePhoneStateListener listener;
 
 	private UserInfo userInfo;
-	private String httpUrl;
 	private SharedPreferences preferences;
 
 	@SuppressWarnings("static-access")
@@ -53,7 +39,6 @@ public class PhoneBroadcastReceiver extends BroadcastReceiver {
 		if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
 			// TODO
 			userInfo = (UserInfo) ctx.getApplicationContext();
-			httpUrl = ctx.getString(R.string.httpUrl);
 			preferences = ctx.getSharedPreferences(AppUtil.UN_UPLOAD,
 					ctx.getApplicationContext().MODE_PRIVATE);
 
@@ -79,96 +64,14 @@ public class PhoneBroadcastReceiver extends BroadcastReceiver {
 		super.finalize();
 	}
 
-	@SuppressLint("HandlerLeak")
-	private Handler handler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			super.handleMessage(msg);
-			switch (msg.what) {
-			case ServiceAction.COMMIT_TASK:
-				commitTask(msg);
-				break;
-			default:
-				break;
-			}
-		}
-
-		private void commitTask(Message msg) {
-			// TODO
-			if (null == msg.obj) {
-				return;
-			}
-
-			// TODO
-			try {
-				JSONObject _jo = new JSONObject((String) msg.obj);
-				// TODO
-				if (!_jo.getBoolean("success"))
-					return;
-
-				// TODO
-				Editor editor = preferences.edit();
-				editor.remove("id");
-				editor.commit();
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-		}
-	};
-
 	private void commitTask(String tel_num, long talk_time, int talk_time_len) {
-		if (null == tel_num
-				|| !tel_num.equals(userInfo.getCallInfo().getTel_num())
-				|| talk_time_len < userInfo.getCallInfo().getTalk_time_len())
-			return;
-
-		HashMap<String, String> _params = new HashMap<String, String>();
 		// TODO
-		_params.put("command", "commitTask");
-		long _ts = (new Date()).getTime() + userInfo.getTs();
-		_params.put("ts", Long.toString(_ts));
-
-		// TODO
-		try {
-			_params.put("apikey",
-					URLEncoder.encode(userInfo.getApikey(), "UTF-8"));
-
-			JSONObject _jo = new JSONObject();
-			_jo.put("id", userInfo.getCallInfo().getHandtask_id());
-			_jo.put("TALK_TIME_LEN", talk_time_len);
-			_jo.put("TALK_TIME", talk_time);
-			_jo.put("TEL_NUM", tel_num);
-
-			// TODO
-			String _data = _jo.toString();
-			_params.put("data", URLEncoder.encode(_data, "UTF-8"));
-
-			String _paramStr = URLEncoder.encode(
-					"apikey=" + userInfo.getApikey()
-							+ "&command=commitTask&data=" + _data + "&ts="
-							+ _ts, "UTF-8");
-			_params.put("signature",
-					RestUtil.standard(_paramStr, userInfo.getSeckey()));
-
-			// TODO
-			Editor editor = preferences.edit();
-			editor.putString("id", userInfo.getCallInfo().getHandtask_id());
-			editor.putInt("TALK_TIME_LEN", talk_time_len);
-			editor.putLong("TALK_TIME", talk_time);
-			editor.putString("TEL_NUM", tel_num);
-			editor.commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		} finally {
-			userInfo.setCallInfo(null);
-		}
-
-		// TODO
-		HttpUtil _hu = new HttpUtil(ServiceAction.COMMIT_TASK, handler, httpUrl
-				+ "api", RequestMethod.POST, _params);
-		Thread _t = new Thread(_hu);
-		_t.start();
+		Editor editor = preferences.edit();
+		editor.putString("id", userInfo.getCallInfo().getHandtask_id());
+		editor.putInt("TALK_TIME_LEN", talk_time_len);
+		editor.putLong("TALK_TIME", talk_time);
+		editor.putString("TEL_NUM", tel_num);
+		editor.commit();
 	}
 
 	private void findLast(Context ctx, String tel_num, long call_time) {
