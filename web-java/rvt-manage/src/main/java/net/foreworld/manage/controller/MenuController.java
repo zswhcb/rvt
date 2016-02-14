65e4dcc38ed523e111614922cb3f2c63207bc270
unknown <3203317@qq.com>
2016-02-14 10:45:41 +0800

    haha

--- web-java/rvt-manage/src/main/java/net/foreworld/manage/controller/MenuController.java
+++ web-java/rvt-manage/src/main/java/net/foreworld/manage/controller/MenuController.java
@@ -1,15 +1,22 @@
 package net.foreworld.manage.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import net.foreworld.manage.model.Menu;
 import net.foreworld.manage.service.MenuService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import freemarker.template.Template;
 
 /**
  *
@@ -24,7 +31,11 @@ public class MenuController {
 	@Autowired
 	private MenuService menuService;
 
+	@Autowired
+	private FreeMarkerConfigurer freemarkerConfigurer;
+
 	private String index_ftl = "menu/1.0.1/index";
+	private String menu_list_ftl = "menu/1.0.1/_pagelet/list.html";
 
 	@RequestMapping(value = { "/menu/" }, method = RequestMethod.GET)
 	public ModelAndView indexUI() {
@@ -38,13 +49,28 @@ public class MenuController {
 	}
 
 	@RequestMapping(value = { "/menu/list" }, method = RequestMethod.POST, produces = "application/json")
-	public ModelAndView list(Menu menu) {
+	public ModelAndView list(Menu menu,
+			@RequestParam(required = false, defaultValue = "1") int json) {
 		ModelAndView result = new ModelAndView();
+		result.addObject("success", true);
 		// TODO
 		List<Menu> list = menuService.findByPid(menu.getPid());
-		result.addObject("data", list);
 		// TODO
-		result.addObject("success", true);
+		if (1 == json) {
+			result.addObject("data", list);
+		} else {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("data_menus", list);
+			// TODO
+			try {
+				Template template = freemarkerConfigurer.getConfiguration()
+						.getTemplate(menu_list_ftl);
+				result.addObject("data", FreeMarkerTemplateUtils
+						.processTemplateIntoString(template, map));
+			} catch (Exception ignore) {
+				result.addObject("success", false);
+			} // END
+		} // END
 		return result;
 	}
 }
