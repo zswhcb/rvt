1de7534dd88c3620cd2f732a1cc1655730e76ec4
3203317 <3203317@qq.com>
2016-02-28 18:43:16 +0800

    haha

--- api/controllers/api.js
+++ api/controllers/api.js
@@ -5,18 +5,18 @@
  */
 'use strict';
 
-var util = require('speedt-utils'),
-	rest = util.service.rest,
-	EventProxy = require('eventproxy'),
-	path = require('path'),
-	fs = require('fs'),
-	cwd = process.cwd();
+var util = require('speedt-utils');
+var rest = util.service.rest;
+var EventProxy = require('eventproxy');
+var path = require('path');
+var fs = require('fs');
+var cwd = process.cwd();
 
 var conf = require('../settings');
 
 var biz = {
-	task: require('../../biz/task'),
-	user: require('../../biz/user')
+    task: require('../../biz/task'),
+    user: require('../../biz/user')
 };
 
 var exports = module.exports;
@@ -29,50 +29,50 @@ var exports = module.exports;
  */
 (function (exports){
 
-	function isEmtpy(str){
-		if(!str) return;
-		str = str.trim();
-		return '' === str ? null : str;
-	}
+    function isEmtpy(str){
+	if(!str) return;
+	str = str.trim();
+	return '' === str ? null : str;
+    }
 
-	exports.signature_validate = function(req, res, next){
-		var result = { success: false },
-			data = req._data;
+    exports.signature_validate = function(req, res, next){
+	var result = { success: false };
+	var data = req._data;
 
-		var body = req.body;
+	var body = req.body;
 
-		// TODO
-		body.command = isEmtpy(body.command);
-		if(!body.command) return res.send(result);
+	// TODO
+	body.command = isEmtpy(body.command);
+	if(!body.command) return res.send(result);
 
-		// TODO
-		if('login' === body.command) return next();
+	// TODO
+	if('login' === body.command) return next();
 
-		body.ts = isEmtpy(body.ts);
-		if(!body.ts) return res.send(result);
+	body.ts = isEmtpy(body.ts);
+	if(!body.ts) return res.send(result);
 
-		body.apikey = isEmtpy(body.apikey);
-		if(!body.apikey) return res.send(result);
+	body.apikey = isEmtpy(body.apikey);
+	if(!body.apikey) return res.send(result);
 
-		body.signature = isEmtpy(body.signature);
-		if(!body.signature) return res.send(result);
+	body.signature = isEmtpy(body.signature);
+	if(!body.signature) return res.send(result);
 
-		// TODO
-		biz.user.findByApiKey(body.apikey, function (err, doc){
-			if(err) return next(err);
-			// TODO
-			if(!doc) return res.send(result);
+	// TODO
+	biz.user.findByApiKey(body.apikey, function (err, doc){
+	    if(err) return next(err);
+	    // TODO
+	    if(!doc) return res.send(result);
 
-			// TODO
-			if(1 !== doc.STATUS) return res.send(result);
+	    // TODO
+	    if(1 !== doc.STATUS) return res.send(result);
 
-			// TODO
-			if(!rest.validate(data, doc.SECKEY)) return res.send(result);
-			// TODO
-			req.flash('user', doc);
-			next();
-		});
-	};
+	    // TODO
+	    if(!rest.validate(data, doc.SECKEY)) return res.send(result);
+	    // TODO
+	    req.flash('user', doc);
+	    next();
+	});
+    };
 })(exports);
 
 /**
@@ -83,71 +83,71 @@ var exports = module.exports;
  */
 (function (exports){
 
-	function login(req, res, next){
-		var result = { success: false },
-			data = req._data;
-		// TODO
-		biz.user.login(data, function (err, msg, doc){
-			if(err) return next(err);
-			if(msg){
-				result.msg = msg;
-				return res.send(result);
-			}
+    function login(req, res, next){
+	var result = { success: false };
+	var data = req._data;
+	// TODO
+	biz.user.login(data, function (err, msg, doc){
+	    if(err) return next(err);
+	    if(msg){
+		result.msg = msg;
+		return res.send(result);
+	    }
 
-			/* result */
-			result.data = {
-				apikey: doc.APIKEY,
-				seckey: doc.SECKEY,
-				ver: conf.app.ver,
-				ts: (new Date()).getTime()
-			};
-			result.success = true;
-			res.send(result);
-		});
+	    /* result */
+	    result.data = {
+		apikey: doc.APIKEY,
+		seckey: doc.SECKEY,
+		ver: conf.app.ver,
+		ts: (new Date()).getTime()
+	    };
+	    result.success = true;
+	    res.send(result);
+	});
+    }
+
+    function applyTask(req, res, next){
+	var result = { success: false };
+	var data = req._data;
+	var user = req.flash('user')[0];
+	// TODO
+	biz.task.apply(user.id, function (err, msg, doc){
+	    if(err) return next(err);
+	    // TODO
+	    if(msg){
+		result.msg = msg;
+		return res.send(result);
+	    }
+	    // TODO
+	    result.data = doc;
+	    result.success = true;
+	    res.send(result);
+	});
+    }
+
+    function commitTask(req, res, next){
+	var result = { success: false };
+	var data = req._data;
+	var user = req.flash('user')[0];
+	// TODO
+	biz.task.commit(user.id, data, function (err, msg, status){
+	    if(err) return next(err);
+	    // TODO
+	    if(msg) result.msg = msg;
+	    result.success = true;
+	    res.send(result);
+	});
+    }
+
+    exports.index = function(req, res, next){
+	// TODO
+	switch(req.body.command){
+	case 'login': login(req, res, next); break;
+	case 'applyTask': applyTask(req, res, next); break;
+	case 'commitTask': commitTask(req, res, next); break;
+	default: res.send({ success: false }); break;
 	}
-
-	function applyTask(req, res, next){
-		var result = { success: false },
-			data = req._data,
-			user = req.flash('user')[0];
-		// TODO
-		biz.task.apply(user.id, function (err, msg, doc){
-			if(err) return next(err);
-			// TODO
-			if(msg){
-				result.msg = msg;
-				return res.send(result);
-			}
-			// TODO
-			result.data = doc;
-			result.success = true;
-			res.send(result);
-		});
-	}
-
-	function commitTask(req, res, next){
-		var result = { success: false },
-			data = req._data,
-			user = req.flash('user')[0];
-		// TODO
-		biz.task.commit(user.id, data, function (err, msg, status){
-			if(err) return next(err);
-			// TODO
-			if(msg) result.msg = msg;
-			result.success = true;
-			res.send(result);
-		});
-	}
-
-	exports.index = function(req, res, next){
-		// TODO
-		switch(req.body.command){
-			case 'login': login(req, res, next); break;
-			case 'applyTask': applyTask(req, res, next); break;
-			case 'commitTask': commitTask(req, res, next); break;
-			default: res.send({ success: false }); break;
-		}
-	};
+    };
 })(exports);
 
 /**
@@ -156,9 +156,9 @@ var exports = module.exports;
  * @return
  */
 exports.testUI = function(req, res, next){
-	res.render('test', {
-		conf: conf,
-		description: '',
-		keywords: ',html5'
-	});
+    res.render('test', {
+	conf: conf,
+	description: '',
+	keywords: ',html5'
+    });
 };
--- biz/task.js
+++ biz/task.js
@@ -6,13 +6,13 @@
 'use strict';
 
 var util = require('speedt-utils'),
-	mysql_util = util.mysql_util,
-	mysql = util.mysql;
+mysql_util = util.mysql_util,
+mysql = util.mysql;
 
 var exports = module.exports;
 
 var biz = {
-	handtask: require('./handtask')
+    handtask: require('./handtask')
 };
 
 /**
@@ -22,34 +22,34 @@ var biz = {
  * @return
  */
 (function (exports){
+    // TODO
+    exports.commit = function(user_id, newInfo, cb){
+	// TODO 检查是否为数字
+	var TALK_TIME_LEN = util.checkNum(newInfo.TALK_TIME_LEN);
+	if(null === TALK_TIME_LEN || 0 === TALK_TIME_LEN) return cb(null, ['参数异常']);
 	// TODO
-	exports.commit = function(user_id, newInfo, cb){
-		// TODO 检查是否为数字
-		var TALK_TIME_LEN = util.checkNum(newInfo.TALK_TIME_LEN);
-		if(null === TALK_TIME_LEN || 0 === TALK_TIME_LEN) return cb(null, ['参数异常']);
-		// TODO
-		biz.handtask.getById(newInfo.id, function (err, doc){
-			if(err) return cb(err);
-			if(!doc ||
-				(1 === doc.HANDTASK_STATUS) ||
-				(user_id !== doc.HANDTASK_USER_ID) ||
-				(newInfo.TEL_NUM !== doc.TEL_NUM)) return cb(null, ['非法操作']);
-			// TODO 通话时长不达标
-			if(TALK_TIME_LEN < doc.TALK_TIME_LEN) return cb(null, ['通话时长不能少于 '+ doc.TALK_TIME_LEN +' 秒']);
-			// TODO 当前时间
-			var curTime = new Date();
-			// TODO 超时截止时间
-			var timeout = new Date(doc.HANDTASK_CREATE_TIME.getTime() + (doc.TALK_TIMEOUT * 1000));
-			// TODO
-			newInfo.STATUS = timeout.getTime() > curTime.getTime() ? 1 : 2;
-			newInfo.TALK_TIME_LEN = TALK_TIME_LEN;
-			// TODO
-			biz.handtask.editInfo(newInfo, function (err, status){
-				if(err) return cb(err);
-				cb(null, null, status);
-			});
-		});
-	};
+	biz.handtask.getById(newInfo.id, function (err, doc){
+	    if(err) return cb(err);
+	    if(!doc ||
+	       (1 === doc.HANDTASK_STATUS) ||
+	       (user_id !== doc.HANDTASK_USER_ID) ||
+	       (newInfo.TEL_NUM !== doc.TEL_NUM)) return cb(null, ['非法操作']);
+	    // TODO 通话时长不达标
+	    if(TALK_TIME_LEN < doc.TALK_TIME_LEN) return cb(null, ['通话时长不能少于 '+ doc.TALK_TIME_LEN +' 秒']);
+	    // TODO 当前时间
+	    var curTime = new Date();
+	    // TODO 超时截止时间
+	    var timeout = new Date(doc.HANDTASK_CREATE_TIME.getTime() + (doc.TALK_TIMEOUT * 1000));
+	    // TODO
+	    newInfo.STATUS = timeout.getTime() > curTime.getTime() ? 1 : 2;
+	    newInfo.TALK_TIME_LEN = TALK_TIME_LEN;
+	    // TODO
+	    biz.handtask.editInfo(newInfo, function (err, status){
+		if(err) return cb(err);
+		cb(null, null, status);
+	    });
+	});
+    };
 })(exports);
 
 /**
@@ -59,37 +59,37 @@ var biz = {
  * @return
  */
 exports.apply = function(user_id, task_id, cb){
-	var that = this;
-	// TODO 清理超时数据
-	biz.handtask.clearTimeout(function (err, status){
+    var that = this;
+    // TODO 清理超时数据
+    biz.handtask.clearTimeout(function (err, status){
+	if(err) return cb(err);
+	// TODO 获取之前申请的任务（未过期）
+	biz.handtask.getMyHandTask(user_id, function (err, msg, doc){
+	    if(err) return cb(err);
+	    if(!!msg) return cb(null, msg);
+	    if(!!doc) return cb(null, null, doc);
+	    // TODO 获取任务状态信息
+	    that.getTaskStatus(user_id, task_id, function (err, msg, doc){
 		if(err) return cb(err);
-		// TODO 获取之前申请的任务（未过期）
-		biz.handtask.getMyHandTask(user_id, function (err, msg, doc){
-			if(err) return cb(err);
-			if(!!msg) return cb(null, msg);
-			if(!!doc) return cb(null, null, doc);
-			// TODO 获取任务状态信息
-			that.getTaskStatus(user_id, task_id, function (err, msg, doc){
-				if(err) return cb(err);
-				if(!!msg) return cb(null, msg);
-				if(!doc) return cb(null, ['此任务不存在，请重新申请']);
-				// TODO
-				if(!!doc.HANDTASK_ID) return cb(null, ['同一项目下的任务只能申请一次']);
-				// TODO 检测任务状态
-				if((doc.INIT_TASK_SUM + doc.SUCCESS_TASK_SUM) >= doc.TASK_SUM) return cb(null, ['下手晚了']);
-				// TODO
-				var newTask = doc;
-				// TODO 开始新的申请
-				biz.handtask.saveNew({ TASK_ID: task_id, USER_ID: user_id }, function (err, doc){
-					if(err) return cb(err);
-					// TODO 返回抢任务的ID
-					newTask.HANDTASK_ID = doc.id;
-					newTask.HANDTASK_CREATE_TIME = doc.CREATE_TIME;
-					cb(null, null, newTask);
-				});
-			});
+		if(!!msg) return cb(null, msg);
+		if(!doc) return cb(null, ['此任务不存在，请重新申请']);
+		// TODO
+		if(!!doc.HANDTASK_ID) return cb(null, ['同一项目下的任务只能申请一次']);
+		// TODO 检测任务状态
+		if((doc.INIT_TASK_SUM + doc.SUCCESS_TASK_SUM) >= doc.TASK_SUM) return cb(null, ['下手晚了']);
+		// TODO
+		var newTask = doc;
+		// TODO 开始新的申请
+		biz.handtask.saveNew({ TASK_ID: task_id, USER_ID: user_id }, function (err, doc){
+		    if(err) return cb(err);
+		    // TODO 返回抢任务的ID
+		    newTask.HANDTASK_ID = doc.id;
+		    newTask.HANDTASK_CREATE_TIME = doc.CREATE_TIME;
+		    cb(null, null, newTask);
 		});
+	    });
 	});
+    });
 };
 
 /**
@@ -99,48 +99,48 @@ exports.apply = function(user_id, task_id, cb){
  * @return
  */
 (function (exports){
-	var sql_1 = 'SELECT'+
-					'  (SELECT COUNT(1) FROM p_handtask WHERE STATUS=0 AND TASK_ID=b.id) INIT_TASK_SUM,'+
-					'  (SELECT COUNT(1) FROM p_handtask WHERE STATUS=1 AND TASK_ID=b.id) SUCCESS_TASK_SUM,'+
-					'  b.*'+
-					' FROM p_task b WHERE ? BETWEEN b.START_TIME AND b.END_TIME';
+    var sql_1 = 'SELECT'+
+	'  (SELECT COUNT(1) FROM p_handtask WHERE STATUS=0 AND TASK_ID=b.id) INIT_TASK_SUM,'+
+	'  (SELECT COUNT(1) FROM p_handtask WHERE STATUS=1 AND TASK_ID=b.id) SUCCESS_TASK_SUM,'+
+	'  b.*'+
+	' FROM p_task b WHERE ? BETWEEN b.START_TIME AND b.END_TIME';
 
-	/**
-	 * 获取指定任务ID的任务状态
-	 *
-	 * @params
-	 * @return
-	 */
-	exports.getTaskStatus = function(user_id, task_id, cb){
-		var sql = 'SELECT'+
-					'  (SELECT a.id FROM p_handtask a WHERE a.STATUS=1 AND a.USER_ID=? AND a.TASK_ID IN (SELECT id FROM p_task WHERE PROJECT_ID=c.PROJECT_ID) LIMIT 1) HANDTASK_ID,'+
-					'  (SELECT COUNT(1) FROM p_handtask a WHERE a.STATUS=1 AND a.USER_ID=? AND a.TASK_ID IN (SELECT id FROM p_task WHERE PROJECT_ID=c.PROJECT_ID)) HANDTASK_FINISH_COUNT,'+
-					'  c.*'+
-					' FROM ('+ sql_1 +') c WHERE c.TASK_SUM>c.SUCCESS_TASK_SUM AND c.id=?';
-		// TODO
-		mysql.query(sql, [user_id, user_id, new Date(), task_id], function (err, docs){
-			if(err) return cb(err);
-			// TODO
-			if(!mysql.checkOnly(docs)) return cb(null, null, null);
-			// TODO
-			var doc = docs[0];
-			if(1 < doc.HANDTASK_FINISH_COUNT) return cb(null, ['数据异常，请联系管理员']);
-			// TODO
-			cb(null, null, doc);
-		});
-	};
-
+    /**
+     * 获取指定任务ID的任务状态
+     *
+     * @params
+     * @return
+     */
+    exports.getTaskStatus = function(user_id, task_id, cb){
+	var sql = 'SELECT'+
+	    '  (SELECT a.id FROM p_handtask a WHERE a.STATUS=1 AND a.USER_ID=? AND a.TASK_ID IN (SELECT id FROM p_task WHERE PROJECT_ID=c.PROJECT_ID) LIMIT 1) HANDTASK_ID,'+
+	    '  (SELECT COUNT(1) FROM p_handtask a WHERE a.STATUS=1 AND a.USER_ID=? AND a.TASK_ID IN (SELECT id FROM p_task WHERE PROJECT_ID=c.PROJECT_ID)) HANDTASK_FINISH_COUNT,'+
+	    '  c.*'+
+	    ' FROM ('+ sql_1 +') c WHERE c.TASK_SUM>c.SUCCESS_TASK_SUM AND c.id=?';
 	// TODO
-	exports.getCurrentTasks = function(user_id, cb){
-		var sql = 'SELECT'+
-					' IFNULL((SELECT d.STATUS FROM p_handtask d LEFT JOIN p_task e ON (d.TASK_ID=e.id) WHERE e.id IS NOT NULL AND e.PROJECT_ID=c.PROJECT_ID AND d.USER_ID=? ORDER BY d.CREATE_TIME DESC LIMIT 1), 4) HANDTASK_STATUS,'+
-					' c.* FROM ('+ sql_1 +') c ORDER BY c.CREATE_TIME DESC';
-		// TODO
-		mysql.query(sql, [user_id, new Date()], function (err, docs){
-			if(err) return cb(err);
-			cb(null, docs);
-		});
-	};
+	mysql.query(sql, [user_id, user_id, new Date(), task_id], function (err, docs){
+	    if(err) return cb(err);
+	    // TODO
+	    if(!mysql.checkOnly(docs)) return cb(null, null, null);
+	    // TODO
+	    var doc = docs[0];
+	    if(1 < doc.HANDTASK_FINISH_COUNT) return cb(null, ['数据异常，请联系管理员']);
+	    // TODO
+	    cb(null, null, doc);
+	});
+    };
+
+    // TODO
+    exports.getCurrentTasks = function(user_id, cb){
+	var sql = 'SELECT'+
+	    ' IFNULL((SELECT d.STATUS FROM p_handtask d LEFT JOIN p_task e ON (d.TASK_ID=e.id) WHERE e.id IS NOT NULL AND e.PROJECT_ID=c.PROJECT_ID AND d.USER_ID=? ORDER BY d.CREATE_TIME DESC LIMIT 1), 4) HANDTASK_STATUS,'+
+	    ' c.* FROM ('+ sql_1 +') c ORDER BY c.CREATE_TIME DESC';
+	// TODO
+	mysql.query(sql, [user_id, new Date()], function (err, docs){
+	    if(err) return cb(err);
+	    cb(null, docs);
+	});
+    };
 })(exports);
 
 /**
@@ -150,63 +150,63 @@ exports.apply = function(user_id, task_id, cb){
  * @return
  */
 (function (exports){
-	var sql = 'DELETE FROM p_task WHERE id=?';
+    var sql = 'DELETE FROM p_task WHERE id=?';
+    // TODO
+    exports.remove = function(id, cb){
 	// TODO
-	exports.remove = function(id, cb){
-		// TODO
-		mysql.query(sql, [id], function (err, status){
-			if(err) return cb(err);
-			cb(null, status);
-		});
-	};
+	mysql.query(sql, [id], function (err, status){
+	    if(err) return cb(err);
+	    cb(null, status);
+	});
+    };
 })(exports);
 
 
 (function (exports){
-	var sql_1 = 'SELECT c.USER_NAME, b.PROJECT_NAME, a.* FROM p_task a LEFT JOIN p_project b ON (a.PROJECT_ID=b.id) LEFT JOIN s_user c ON (a.CREATE_USER_ID=c.id) WHERE b.id IS NOT NULL AND c.id IS NOT NULL';
-	var sql_orderby = ' ORDER BY a.CREATE_TIME DESC';
+    var sql_1 = 'SELECT c.USER_NAME, b.PROJECT_NAME, a.* FROM p_task a LEFT JOIN p_project b ON (a.PROJECT_ID=b.id) LEFT JOIN s_user c ON (a.CREATE_USER_ID=c.id) WHERE b.id IS NOT NULL AND c.id IS NOT NULL';
+    var sql_orderby = ' ORDER BY a.CREATE_TIME DESC';
 
-	/**
-	 *
-	 * @params
-	 * @return
-	 */
-	exports.getById = function(id, cb){
-		var sql = sql_1 +' AND a.id=?';
-		// TODO
-		mysql.query(sql, [id], function (err, docs){
-			if(err) return cb(err);
-			cb(null, mysql.checkOnly(docs) ? docs[0] : null);
-		});
-	};
+    /**
+     *
+     * @params
+     * @return
+     */
+    exports.getById = function(id, cb){
+	var sql = sql_1 +' AND a.id=?';
+	// TODO
+	mysql.query(sql, [id], function (err, docs){
+	    if(err) return cb(err);
+	    cb(null, mysql.checkOnly(docs) ? docs[0] : null);
+	});
+    };
 
-	/**
-	 *
-	 * @params
-	 * @return
-	 */
-	exports.findAll = function(cb){
-		var sql = sql_1 + sql_orderby;
-		// TODO
-		mysql.query(sql, null, function (err, docs){
-			if(err) return cb(err);
-			cb(null, docs);
-		});
-	};
+    /**
+     *
+     * @params
+     * @return
+     */
+    exports.findAll = function(cb){
+	var sql = sql_1 + sql_orderby;
+	// TODO
+	mysql.query(sql, null, function (err, docs){
+	    if(err) return cb(err);
+	    cb(null, docs);
+	});
+    };
 
-	/**
-	 *
-	 * @params
-	 * @return
-	 */
-	exports.findByProjectId = function(project_id, cb){
-		var sql = sql_1 +' AND a.PROJECT_ID=?'+ sql_orderby;
-		// TODO
-		mysql.query(sql, [project_id], function (err, docs){
-			if(err) return cb(err);
-			cb(null, docs);
-		});
-	};
+    /**
+     *
+     * @params
+     * @return
+     */
+    exports.findByProjectId = function(project_id, cb){
+	var sql = sql_1 +' AND a.PROJECT_ID=?'+ sql_orderby;
+	// TODO
+	mysql.query(sql, [project_id], function (err, docs){
+	    if(err) return cb(err);
+	    cb(null, docs);
+	});
+    };
 })(exports);
 
 /**
@@ -216,74 +216,74 @@ exports.apply = function(user_id, task_id, cb){
  * @return
  */
 (function (exports){
-	function formVali(newInfo, cb){
-		cb(null);
-	}
+    function formVali(newInfo, cb){
+	cb(null);
+    }
 
-	/**
-	 * 添加
-	 *
-	 * @params
-	 * @return
-	 */
-	(function (exports){
-		var sql = 'INSERT INTO p_task (id, TASK_NAME, TASK_INTRO, SMS_INTRO, TASK_SUM, PROJECT_ID, TALK_TIME_LEN, TALK_TIMEOUT, START_TIME, END_TIME, CREATE_USER_ID, CREATE_TIME) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-		// TODO
-		exports.saveNew = function(newInfo, cb){
-			formVali(newInfo, function (err){
-				if(err) return cb(err);
-				// CREATE
-				var postData = [
-					util.genObjectId(),
-					newInfo.TASK_NAME,
-					newInfo.TASK_INTRO,
-					newInfo.SMS_INTRO,
-					newInfo.TASK_SUM || 20,
-					newInfo.PROJECT_ID,
-					newInfo.TALK_TIME_LEN || 30,
-					newInfo.TALK_TIMEOUT || 3600,
-					newInfo.START_TIME || new Date(),
-					newInfo.END_TIME || new Date(),
-					newInfo.CREATE_USER_ID,
-					new Date()
-				];
-				mysql.query(sql, postData, function (err, status){
-					if(err) return cb(err);
-					cb(null, null, status);
-				});
-			});
-		};
-	})(exports);
+    /**
+     * 添加
+     *
+     * @params
+     * @return
+     */
+    (function (exports){
+	var sql = 'INSERT INTO p_task (id, TASK_NAME, TASK_INTRO, SMS_INTRO, TASK_SUM, PROJECT_ID, TALK_TIME_LEN, TALK_TIMEOUT, START_TIME, END_TIME, CREATE_USER_ID, CREATE_TIME) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+	// TODO
+	exports.saveNew = function(newInfo, cb){
+	    formVali(newInfo, function (err){
+		if(err) return cb(err);
+		// CREATE
+		var postData = [
+		    util.genObjectId(),
+		    newInfo.TASK_NAME,
+		    newInfo.TASK_INTRO,
+		    newInfo.SMS_INTRO,
+		    newInfo.TASK_SUM || 20,
+		    newInfo.PROJECT_ID,
+		    newInfo.TALK_TIME_LEN || 30,
+		    newInfo.TALK_TIMEOUT || 3600,
+		    newInfo.START_TIME || new Date(),
+		    newInfo.END_TIME || new Date(),
+		    newInfo.CREATE_USER_ID,
+		    new Date()
+		];
+		mysql.query(sql, postData, function (err, status){
+		    if(err) return cb(err);
+		    cb(null, null, status);
+		});
+	    });
+	};
+    })(exports);
 
-	/**
-	 * 修改
-	 *
-	 * @params
-	 * @return
-	 */
-	(function (exports){
-		var sql = 'UPDATE p_task set TASK_NAME=?, TASK_INTRO=?, SMS_INTRO=?, TASK_SUM=?, TALK_TIME_LEN=?, TALK_TIMEOUT=?, START_TIME=?, END_TIME=? WHERE id=?';
-		// TODO
-		exports.editInfo = function(newInfo, cb){
-			formVali(newInfo, function (err){
-				if(err) return cb(err);
-				// CREATE
-				var postData = [
-					newInfo.TASK_NAME,
-					newInfo.TASK_INTRO,
-					newInfo.SMS_INTRO,
-					newInfo.TASK_SUM || 20,
-					newInfo.TALK_TIME_LEN || 30,
-					newInfo.TALK_TIMEOUT || 3600,
-					newInfo.START_TIME || new Date(),
-					newInfo.END_TIME || new Date(),
-					newInfo.id
-				];
-				mysql.query(sql, postData, function (err, status){
-					if(err) return cb(err);
-					cb(null, null, status);
-				});
-			});
-		};
-	})(exports);
+    /**
+     * 修改
+     *
+     * @params
+     * @return
+     */
+    (function (exports){
+	var sql = 'UPDATE p_task set TASK_NAME=?, TASK_INTRO=?, SMS_INTRO=?, TASK_SUM=?, TALK_TIME_LEN=?, TALK_TIMEOUT=?, START_TIME=?, END_TIME=? WHERE id=?';
+	// TODO
+	exports.editInfo = function(newInfo, cb){
+	    formVali(newInfo, function (err){
+		if(err) return cb(err);
+		// CREATE
+		var postData = [
+		    newInfo.TASK_NAME,
+		    newInfo.TASK_INTRO,
+		    newInfo.SMS_INTRO,
+		    newInfo.TASK_SUM || 20,
+		    newInfo.TALK_TIME_LEN || 30,
+		    newInfo.TALK_TIMEOUT || 3600,
+		    newInfo.START_TIME || new Date(),
+		    newInfo.END_TIME || new Date(),
+		    newInfo.id
+		];
+		mysql.query(sql, postData, function (err, status){
+		    if(err) return cb(err);
+		    cb(null, null, status);
+		});
+	    });
+	};
+    })(exports);
 })(exports);
