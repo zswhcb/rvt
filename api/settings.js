0889381c03cb9e1d08ae32829e2785070f230980
ubuntu-1 <ubuntu-1@ubuntu1-VirtualBox.(none)>
2016-02-28 17:24:50 +0800

    haha

--- api/settings.js
+++ api/settings.js
@@ -6,32 +6,32 @@
 'use strict';
 
 module.exports = {
-	cookie: {
-		secret: 'hnzswh-rvt-api'
-	}, corp: {
-		name: '打电话',
-		website: 'http://www.dolalive.com/'
-	}, mysql: {
-		database: 'rvt-manage',
-		host: '127.0.0.1',
-		port: 22306,
-		user: 'root',
-		password: 'password',
-		connectionLimit: 50
-	}, html: {
-		cdn: 'http://www.foreworld.net/',
-		pagesize: 10,
-		cache_time: 1000 * 3
-	}, mail: {
-		secureConnection: true,
-		host: 'smtp.163.com',
-		port: 465,
-		to: ['huangxin@foreworld.net'],
-		auth: {
-			user: 'firefrog@163.com',
-			pass: ''
-		}
-	}, app: {
-		ver: 104
+    cookie: {
+	secret: 'hnzswh-rvt-api'
+    }, corp: {
+	name: '打电话',
+	website: 'http://www.dolalive.com/'
+    }, mysql: {
+	database: 'rvt-manage',
+	host: '192.168.56.1',
+	port: 22306,
+	user: 'root',
+	password: 'password',
+	connectionLimit: 50,
+    }, html: {
+	cdn: 'http://www.foreworld.net/',
+	pagesize: 10,
+	cache_time: 1000 * 3
+    }, mail: {
+	secureConnection: true,
+	host: 'smtp.163.com',
+	port: 465,
+	to: ['huangxin@foreworld.net'],
+	auth: {
+	    user: 'firefrog@163.com',
+	    pass: ''
 	}
+    }, app: {
+	ver: 104
+    }
 };
