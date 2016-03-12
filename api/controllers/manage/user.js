194f1b159c8eb7d95431c5d705e5369d267b1381
unknown <3203317@qq.com>
2016-03-12 11:21:44 +0800

    haha

--- api/controllers/manage/user.js
+++ api/controllers/manage/user.js
@@ -19,6 +19,28 @@ var biz = {
  * @params
  * @return
  */
+exports.remove = function(req, res, next){
+	var data = req._data;
+	// TODO
+	biz.user.remove(data, function (err, msg, status){
+		if(err) return next(err);
+		var result = { success: false };
+
+		if(msg){
+			result.msg = msg;
+			return res.send(result);
+		}
+
+		result.success = true;
+		res.send(result);
+	});
+};
+
+/**
+ *
+ * @params
+ * @return
+ */
 exports.edit = function(req, res, next){
 	var data = req._data;
 	// TODO
--- biz/user.js
+++ biz/user.js
@@ -19,65 +19,6 @@ var biz = {
 	authcode: require('./authcode')
 };
 
-/**
- *
- * @params
- * @return
- */
-exports.findByApiKey = function(apiKey, cb){
-	// TODO
-	mysql_util.find(null, 's_user', [['APIKEY', '=', apiKey]], null, null, function (err, docs){
-		if(err) return cb(err);
-		cb(null, mysql.checkOnly(docs) ? docs[0] : null);
-	});
-};
-
-/**
- * 通过父Id查询子用户
- *
- * @params
- * @return
- */
-(function (exports){
-	var sql = 'SELECT b.* FROM'+
-				' (SELECT'+
-					'  (SELECT COUNT(1) FROM s_auth_code WHERE USER_ID=a.id) AUTH_CODE_COUNT,'+
-					'  IFNULL((SELECT USER_ID FROM s_auth_code WHERE id=a.AUTH_CODE_ID), "") PID,'+
-					'  a.*'+
-					' FROM s_user a WHERE a.STATUS=1 AND a.ROLE_ID="566512b49012fb044691ace6") b'+
-				' WHERE b.PID=?'+
-				' ORDER BY b.CREATE_TIME DESC';
-	// TODO
-	exports.findByPId = function(pid, cb){
-		pid = pid || '';
-		// TODO
-		mysql.query(sql, [pid], function (err, docs){
-			if(err) return cb(err);
-			cb(null, docs);
-		});
-	};
-})(exports);
-
-/**
- * 根据角色查询用户
- *
- * @params
- * @return
- */
-(function (exports){
-	var sql = 'SELECT'+
-				'  (SELECT COUNT(1) FROM p_project WHERE CREATE_USER_ID=a.id) PROJECT_COUNT,'+
-				'  a.*'+
-				' FROM s_user a where a.ROLE_ID=? ORDER BY a.CREATE_TIME DESC';
-	// TODO
-	exports.findByRoleId = function(role_id, cb){
-		mysql.query(sql, [role_id], function (err, docs){
-			if(err) return cb(err);
-			cb(null, docs);
-		});
-	};
-})(exports);
-
 (function (exports){
 	// 查询用户 关联用户角色表
 	var _sql = 'SELECT b.ROLE_NAME, a.* FROM s_user a LEFT JOIN s_role b ON (a.ROLE_ID=b.id) WHERE b.id IS NOT NULL';
@@ -118,12 +59,12 @@ exports.findByApiKey = function(apiKey, cb){
 	 */
 	exports.login = function(logInfo, cb){
 		logInfo.USER_NAME = util.isEmpty(logInfo.USER_NAME);
-		if(!logInfo.USER_NAME) return cb(null, ['用户名或密码输入错误', 'USER_NAME']);
+		if(!logInfo.USER_NAME) return cb(null, ['用户名或密码输入错误']);
 
 		this.findByName(logInfo.USER_NAME, function (err, doc){
 			if(err) return cb(err);
 			// TODO
-			if(!doc) return cb(null, ['用户名或密码输入错误', 'USER_NAME']);
+			if(!doc) return cb(null, ['用户名或密码输入错误']);
 
 			// TODO
 			if(1 !== doc.STATUS) return cb(null, ['禁止登陆']);
@@ -148,48 +89,33 @@ exports.findByApiKey = function(apiKey, cb){
 			cb(null, mysql.checkOnly(docs) ? docs[0] : null);
 		});
 	};
+
+	/**
+	 *
+	 * @params
+	 * @return
+	 */
+	exports.findByName = function(user_name, cb){
+		var sql = _sql +' AND a.USER_NAME=?';
+		mysql.query(sql, [user_name], function (err, docs){
+			if(err) return cb(err);
+			cb(null, mysql.checkOnly(docs) ? docs[0] : null);
+		});
+	};
+
+	/**
+	 *
+	 * @params
+	 * @return
+	 */
+	exports.findByApiKey = function(apiKey, cb){
+		var sql = _sql +' AND a.APIKEY=?';
+		mysql.query(sql, [apiKey], function (err, docs){
+			if(err) return cb(err);
+			cb(null, mysql.checkOnly(docs) ? docs[0] : null);
+		});
+	};
 })(exports);
-
-/**
- *
- * @params
- * @return
- */
-exports.register = function(newInfo, cb){
-	var that = this;
-	// TODO
-	var mobile = util.checkMobile(newInfo.USER_NAME);
-	if('' === mobile) return cb(null, ['请输入正确的手机号']);
-	// TODO
-	newInfo.AUTH_CODE_ID = newInfo.AUTH_CODE_ID || '';
-	// TODO
-	biz.authcode.checkUsed(newInfo.AUTH_CODE_ID, function (err, msg, result, doc){
-		if(err) return cb(err);
-		// TODO
-		if(!doc) return cb(null, msg);
-		if(result) return cb(null, msg);
-		// TODO
-		newInfo.USER_NAME = mobile;
-		newInfo.MOBILE = newInfo.USER_NAME;
-		newInfo.ROLE_ID = '566512b49012fb044691ace6';
-		newInfo.STATUS = 2;
-		that.saveNew(newInfo, cb);
-	});
-};
-
-/**
- * 查找用户通过用户名
- *
- * @params
- * @return
- */
-exports.findByName = function(name, cb){
-	// TODO
-	mysql_util.find(null, 's_user', [['USER_NAME', '=', name]], null, null, function (err, docs){
-		if(err) return cb(err);
-		cb(null, mysql.checkOnly(docs) ? docs[0] : null);
-	});
-};
 
 /**
  * 表单
@@ -274,4 +200,29 @@ exports.findByName = function(name, cb){
 			});
 		};
 	})(exports);
+})(exports);
+
+/**
+ *
+ * @params
+ * @return
+ */
+(function (exports){
+	var _sql_start = 'DELETE FROM s_user WHERE id IN (';
+	var _sql_end = ')';
+
+	// TODO
+	exports.remove = function(ids, cb){
+		if(!ids && (0 === ids.length)) return cb(null, ['参数异常']);
+
+		var sql = _sql_start;
+		for(var i in ids){ sql += '?, '; }
+		sql = sql.substring(0, sql.length-2);
+		sql += _sql_end;
+
+		mysql.query(sql, ids, function (err, status){
+			if(err) return cb(err);
+			cb(null, null, status);
+		});
+	};
 })(exports);
